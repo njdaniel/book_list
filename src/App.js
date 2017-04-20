385b5972846f5fba285f05d130a53000bd8238f5
njdaniel <nicholasjdaniel@gmail.com>
2017-04-20 16:14:25 -0400

    add containers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BookList from './containers/book-list';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           Creating an app with redux
         </p>
+        <BookList/>
       </div>
     );
   }
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -2,8 +2,9 @@
  * Created by nicholas on 4/20/17.
  */
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
 
     renderList() {
         return this.props.books.map((book) => {
@@ -22,3 +23,13 @@ export default class BookList extends Component {
         )
     }
 }
+
+function mapStateToProps(state) {
+    // Whatever is returned will show up as props
+    // inside of BookList
+    return {
+        books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
