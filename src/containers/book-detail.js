ebcc75cd64d32ddbfc89c037378a3898227ad964
njdaniel <nicholasjdaniel@gmail.com>
2017-04-20 21:23:18 -0400

    add flow for initial bookdetail

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -4,10 +4,17 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
     render() {
+        if(!this.props.book) {
+            return <div>Select a book to get started.</div>
+        }
+
         return (
-            <div>Book Detail!</div>
+            <div>
+                <h3>Details for:</h3>
+                <div>{this.props.book.title}</div>
+            </div>
         );
     }
 }
